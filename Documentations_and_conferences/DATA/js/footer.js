a1c426755bdd1ee78c2e988a3b14a70dbb8c7af2
Clement Cole <clementacole75@gmail.com>
2017-06-19 12:53:26 -0500

    Adding Documentations and Conference Papers

---
+++ Documentations_and_conferences/DATA/js/footer.js
@@ -0,0 +1,5 @@
+// -- FOOTER --
+document.write("<div class=\"footer\">")
+document.write("  <p><a href=\"http://www.ruhr-uni-bochum.de/heart2017/\">HEART 2017</a></p>")
+document.write("  <p>Template by <a href=\"http://1234.info/webtemplates/\">1234.info</a> | W3C XHTML 1.0 | W3C CSS 2.1</p>")
+document.write("</div>")
---
+++ Documentations_and_conferences/DATA/js/header.js
@@ -0,0 +1,4 @@
+// Header banner --
+document.write("<div><img class=\"img-header\" src=\"./img/heart2017.png\" alt=\"\"/></div>")
+// Buffer after header
+document.write("<div class=\"buffer\"></div>")
